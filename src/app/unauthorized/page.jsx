878744efd033166a8c6c6a8816c6badabea7c2c9
Kalifa <128218748+Kalifa-A@users.noreply.github.com>
2026-01-19 21:51:58 +0530

    Delete src directory

--- src/app/unauthorized/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function Unauthorized() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-xl font-bold text-red-600">
-        🚫 403 - Access Denied
-      </h1>
-    </div>
-  );
-}
